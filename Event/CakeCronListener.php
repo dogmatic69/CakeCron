7f729a785e3dd53e5df0e6df4e2a2a7f6ac5a425
dogmatic69 <dogmatic69@gmail.com>
2015-06-03 10:42:04 +0100

    copy the shell dispatch from cake so shells can be dispatched from the cron events

--- Event/CakeCronListener.php
+++ Event/CakeCronListener.php
@@ -67,6 +67,37 @@ class CakeCronListener implements CakeEventListener {
 	}
 
 /**
+ * Dispatch a command to another Shell. Similar to Object::requestAction()
+ * but intended for running shells from other shells.
+ *
+ * ### Usage:
+ *
+ * With a string command:
+ *
+ *	`return $this->dispatchShell('schema create DbAcl');`
+ *
+ * Avoid using this form if you have string arguments, with spaces in them.
+ * The dispatched will be invoked incorrectly. Only use this form for simple
+ * command dispatching.
+ *
+ * With an array command:
+ *
+ * `return $this->dispatchShell('schema', 'create', 'i18n', '--dry');`
+ *
+ * @return mixed The return of the other shell.
+ * @link http://book.cakephp.org/2.0/en/console-and-shells.html#Shell::dispatchShell
+ */
+	public function dispatchShell() {
+		$args = func_get_args();
+		if (is_string($args[0]) && count($args) === 1) {
+			$args = explode(' ', $args[0]);
+		}
+
+		$Dispatcher = new ShellDispatcher($args, false);
+		return $Dispatcher->dispatch();
+	}
+
+/**
  * Before event
  *
  * @param Even $event the even being triggered
